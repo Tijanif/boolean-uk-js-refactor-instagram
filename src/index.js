66470795b4f57415f615e905f185aad1e452abbd
Tijani Fulani <Fulani07@gmail.com>
2021-05-27 17:09:12 +0100

    .

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ let state = {
 // ROOT
 const rootEl = document.querySelector('#root');
 
+// Get Users from server
 const getUsersFromDb = () => {
   return fetch('http://localhost:3000/users').then((response) =>
     response.json()
@@ -23,45 +24,15 @@ function setState(newState) {
   render();
 }
 // HEADER SECTION
-
 const headerEl = document.createElement('header');
 headerEl.className = 'main-header';
 const wrapperEl = document.createElement('div');
 wrapperEl.classList = 'wrapper';
-{
-  /* <header class='main-header'>
-  <div class='wrapper'>
-    <div class='chip active'>
-      <div class='avatar-small'>
-        <img
-          src='https://uploads5.wikiart.org/images/salvador-dali.jpg!Portrait.jpg'
-          alt='Salvador Dali'
-        />
-      </div>
-      <span>Salvador Dali</span>
-    </div>
-    <div class='chip'>
-      <div class='avatar-small'>
-        <img
-          src='https://www.sartle.com/sites/default/files/images/artist/pablo-picasso-137216-5115406.jpg'
-          alt='Picasso'
-        />
-      </div>
-      <span>Picasso</span>
-    </div>
-    <div class='chip'>
-      <div class='avatar-small'>
-        <img
-          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3K588mpXWsXuFcE26ZsuTRN2IeFeKCub8hA&amp;usqp=CAU'
-          alt='Van Gogh'
-        />
-      </div>
-      <span>Van Gogh</span>
-    </div>
-  </div>
-</header>; */
-}
 
+headerEl.append(wrapperEl);
+rootEl.append(headerEl);
+
+// Creating user card
 const createUserCard = (user) => {
   const userCard = document.createElement('div');
   userCard.className = 'chip';
@@ -82,16 +53,13 @@ const createUserCard = (user) => {
   return userCard;
 };
 
+// Render user cards
 const renderUserCard = (state) => {
   for (const user of state.users) {
     let userCardEl = createUserCard(user);
     wrapperEl.append(userCardEl);
   }
 };
-
-headerEl.append(wrapperEl);
-
-rootEl.append(headerEl);
 
 const render = () => {
   renderUserCard(state);
